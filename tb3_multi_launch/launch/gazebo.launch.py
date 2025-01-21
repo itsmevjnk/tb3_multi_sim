fbdc2c8e43671c0f55edce0a690f68e45db20c68
Vinh Nguyen <ngtv0404@gmail.com>
2025-01-22 10:57:32 +1100

    add launch files for launching multiple robots
    TODO: fix entity names

---
+++ tb3_multi_launch/launch/gazebo.launch.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Joep Tool
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, OpaqueFunction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+WORLDS = {
+    'empty': 'empty_world.world',
+    'house': 'turtlebot3_house.world',
+    'world': 'turtlebot3_world.world',
+    'dqn1': 'turtlebot3_dqn_stage1.world',
+    'dqn2': 'turtlebot3_dqn_stage2.world',
+    'dqn3': 'turtlebot3_dqn_stage3.world',
+    'dqn4': 'turtlebot3_dqn_stage4.world',
+}
+
+def launch_gzserver(context, pkg_gazebo_ros):
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        WORLDS[LaunchConfiguration('world', default='empty').perform(context)]
+    )
+
+    return [
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+            ),
+            launch_arguments={'world': world}.items()
+        )
+    ]
+
+def generate_launch_description():
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    return LaunchDescription([
+        OpaqueFunction(function=launch_gzserver, args=[pkg_gazebo_ros]),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            )
+        )
+    ])
---
+++ tb3_multi_launch/launch/tb3_spawner.launch.py
@@ -0,0 +1,78 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def launch_spawner(context):
+    TURTLEBOT3_MODEL = LaunchConfiguration('model', default='waffle').perform(context)
+    model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
+    urdf_path = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'models',
+        model_folder,
+        'model.sdf'
+    )
+
+    x_pose = LaunchConfiguration('x_pose', default='0.0').perform(context)
+    y_pose = LaunchConfiguration('y_pose', default='0.0').perform(context)
+    namespace = LaunchConfiguration('namespace').perform(context)
+    
+    return [
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name=f'{namespace}_spawner',
+            arguments=[
+                '-entity', TURTLEBOT3_MODEL,
+                '-file', urdf_path,
+                '-x', x_pose,
+                '-y', y_pose,
+                '-z', '0.01',
+                '-robot_namespace', namespace
+            ],
+            output='screen'
+        )
+    ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'x_pose', default_value='0.0',
+            description='Specify namespace of the robot'
+        ),
+
+        DeclareLaunchArgument(
+            'y_pose', default_value='0.0',
+            description='Specify namespace of the robot'
+        ),
+
+        DeclareLaunchArgument(
+            'model',
+            default_value='waffle',
+            description='The Turtlebot 3 model to be used (waffle, burger or waffle_pi)'
+        ),
+
+        DeclareLaunchArgument(
+            'namespace',
+            description='The namespace of the robot to be spawned'
+        ),
+
+        OpaqueFunction(function=launch_spawner)
+    ])
